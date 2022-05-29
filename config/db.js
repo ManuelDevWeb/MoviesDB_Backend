caacddf80db36fd46b5a4ffc9ce130f88ac524ab
ManuelDev <manueldeveloper17@gmail.com>
2022-05-29 04:03:06 -0500

    Se agrego la conexion a la DB

---
+++ config/db.js
@@ -0,0 +1,24 @@
+// Importando ODM mongoose
+import mongoose from "mongoose";
+
+// Funcion para conectar a la base de datos
+const connectDB = async () => {
+  try {
+    // Conectandonos a la DB con mongoose
+    const connection = await mongoose.connect(
+      "mongodb+srv://root:admin123@cluster0.tmlpd.mongodb.net/moviesdb?retryWrites=true&w=majority",
+      { useNewUrlParser: true }
+    );
+
+    // Almacenando la info de la conexion de la DB
+    const url = `${connection.connection.host}:${connection.connection.port}`;
+    console.log(`Conectado a la DB: ${url}`);
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    // Forzar cierre del proceso
+    process.exit(1);
+  }
+};
+
+// Exportando funcion connectDB
+export { connectDB };
--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 // Importando express
 import express from "express";
 
+// Importando funcion para conextarnos a la DB
+import { connectDB } from "./config/db.js";
+
 // Puerto
 const PORT = 4000;
 
 // Inicializando aplicacion con express
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.status(200).json({
-    msg: "Hola Mundo",
-  });
-});
+// Llamando a la funcion connectDB
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
